c158a9f7b9b5b17b35dd709ef182bba249da7375
gwongibeom <k201gun@kakao.com>
2024-05-09 19:59:39 +0900

    (개발) 버튼 스타일링 수정

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,12 +4,12 @@ interface Props {
   title: string;
   type?: 'button' | 'submit' | 'reset';
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
-  style?: string;
+  style?: 'POSITIVE' | 'NEGATIVE' | '';
 }
 
-const Button = ({ title, type = 'button', onClick, style }: Props) => {
+const Button = ({ title, type = 'button', onClick, style = '' }: Props) => {
   return (
-    <button onClick={onClick} type={`${type ?? 'button'}`} className={`Button Button_${style}`}>
+    <button onClick={onClick} type={`${type}`} className={`Button Button_${style}`}>
       {title}
     </button>
   );
