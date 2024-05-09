e1caa86b2cf5d3d4b5d037086dec20d2b2a56dfe
gwongibeom <k201gun@kakao.com>
2024-05-09 11:02:21 +0900

    (개발) 라우팅 세팅 및 페이지 컴포넌트 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import { NavLink, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import About from './pages/About'
 import Lottery from './pages/Lottery'
+import Header from './components/Header'
 
 function App() {
   return (
     <>
-      <h1>네브</h1>
-      <NavLink to={'/about'}>어바웃</NavLink>
-      <NavLink to={'/lottery'}>추첨</NavLink>
-      <Routes>
-        <Route path='/about' element={<About />} />
-        <Route path='/lottery' element={<Lottery />} />
-      </Routes>
+      <Header />
+      <div className='route_wrapper'>
+        <Routes>
+          <Route path='/about' element={<About />} />
+          <Route path='/lottery' element={<Lottery />} />
+        </Routes>
+      </div>
     </>
   )
 }
