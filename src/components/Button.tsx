8735cb8bdff79171b62668c4ad86fd27ce06a74d
gwongibeom <k201gun@kakao.com>
2024-05-09 19:15:19 +0900

    (개발) 버튼 스타일링

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,22 +1,18 @@
-import './Button.css'
+import './Button.css';
 
 interface Props {
-  title: string
-  type?: 'button' | 'submit' | 'reset'
-  onClick?: React.MouseEventHandler<HTMLButtonElement>
-  style?: string
+  title: string;
+  type?: 'button' | 'submit' | 'reset';
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  style?: string;
 }
 
 const Button = ({ title, type = 'button', onClick, style }: Props) => {
   return (
-    <button
-      onClick={onClick}
-      type={`${type ?? 'button'}`}
-      className={`Button Button_${style}`}
-    >
+    <button onClick={onClick} type={`${type ?? 'button'}`} className={`Button Button_${style}`}>
       {title}
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
