28e57c5df7992d1122941ec1b07700859aa1830f
gwongibeom <k201gun@kakao.com>
2024-05-10 17:20:03 +0900

    (개발) 인터페이스 생성, Ref 생성, 간단하게 API 리퀘스트 요청 기능 개발

--- src/components/LotteryForm.tsx
+++ src/components/LotteryForm.tsx
@@ -5,34 +5,80 @@ import { useNavigate } from 'react-router-dom';
 import Button from './Button';
 import './LotteryForm.css';
 import useCheckPageId from '../hooks/useCheckPageId';
+import useGetPageComments from '../hooks/useGetPageComments';
 
-interface LotteryForm {
+interface ILotteryForm {
   pageNumber: string | number;
   drawNumber: string | number;
 }
 
+interface IComment {
+  content: string;
+  create_time: string;
+  email: string;
+  id: number;
+  name: string;
+}
+
+interface IData {
+  comment_list: IComment[];
+  total: number;
+}
+
+interface ICommentAPIResult {
+  code: string;
+  data?: IData;
+}
+
 const LotteryForm = () => {
-  const pageNameRef = useRef<HTMLParagraphElement>(null);
+  const pageNameParagraphRef = useRef<HTMLParagraphElement>(null);
+  const pageNumberInput = useRef<HTMLInputElement>(null);
+  const drawNumberInput = useRef<HTMLInputElement>(null);
   const nav = useNavigate();
 
-  const [lotteryForm, setLotteryForm] = useState<LotteryForm>({ pageNumber: '', drawNumber: '' });
+  const [lotteryForm, setLotteryForm] = useState<ILotteryForm>({ pageNumber: '', drawNumber: '' });
 
   const onGoAboutClick = () => {
     nav('/about');
   };
 
   const onPageCheckButton = async () => {
-    if (pageNameRef.current) {
-      pageNameRef.current.style.color = 'rgba(0,0,0,0.6)';
-      pageNameRef.current.textContent = '조회중!';
+    if (pageNameParagraphRef.current) {
+      pageNameParagraphRef.current.style.color = 'rgba(0,0,0,0.6)';
+      pageNameParagraphRef.current.textContent = '조회중!';
       const result = await useCheckPageId(lotteryForm.pageNumber);
-      pageNameRef.current.textContent = result.title;
-      pageNameRef.current.style.color = result.code === 'success' ? 'rgb(100, 201, 101)' : 'rgb(253, 86, 95)';
+      pageNameParagraphRef.current.textContent = result.title;
+      pageNameParagraphRef.current.style.color = result.code === 'success' ? 'rgb(100, 201, 101)' : 'rgb(253, 86, 95)';
     }
   };
 
-  const onSubmit = () => {
-    console.log(lotteryForm);
+  const onSubmit = async () => {
+    const { pageNumber, drawNumber } = lotteryForm;
+    if (pageNumber === '') {
+      pageNumberInput.current?.focus();
+      return;
+    }
+    if (drawNumber === '') {
+      drawNumberInput.current?.focus();
+      return;
+    }
+
+    const response: ICommentAPIResult = await useGetPageComments(pageNumber);
+    if (response.code !== 'success' || !response.data) {
+      alert('불러오기를 실패했어요!');
+      return;
+    }
+
+    const comment_list: IComment[] = response.data.comment_list;
+    if (!comment_list || comment_list.length === 0 || Number(comment_list.length) < Number(drawNumber)) {
+      alert('댓글이 없거나 부족해요!');
+      return;
+    }
+
+    const shuffledComments = comment_list.sort(() => Math.random() - 0.5);
+    const selectedComments = shuffledComments.slice(0, Number(drawNumber));
+
+    console.log(selectedComments);
   };
 
   const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -50,8 +96,8 @@ const LotteryForm = () => {
 
   const onInputReset = () => {
     setLotteryForm({ pageNumber: '', drawNumber: '' });
-    if (pageNameRef.current) {
-      pageNameRef.current.textContent = '';
+    if (pageNameParagraphRef.current) {
+      pageNameParagraphRef.current.textContent = '';
     }
   };
 
@@ -63,6 +109,7 @@ const LotteryForm = () => {
           <input
             type='number'
             name='pageNumber'
+            ref={pageNumberInput}
             value={lotteryForm.pageNumber}
             placeholder='페이지 번호를 입력 해주세요.'
             onChange={onInputChange}
@@ -70,12 +117,19 @@ const LotteryForm = () => {
           />
           <Button title='확인' onClick={onPageCheckButton} />
         </div>
-        <p ref={pageNameRef} className='pageNameRef'></p>
+        <p ref={pageNameParagraphRef} className='pageNameParagraphRef'></p>
       </section>
       <section>
         <h4>추첨 인원</h4>
         <div className='input_wrapper'>
-          <input type='number' name='drawNumber' value={lotteryForm.drawNumber} placeholder='추천인원 수를 입력 해주세요.' onChange={onInputChange} />
+          <input
+            type='number'
+            name='drawNumber'
+            ref={drawNumberInput}
+            value={lotteryForm.drawNumber}
+            placeholder='추천인원 수를 입력 해주세요.'
+            onChange={onInputChange}
+          />
         </div>
       </section>
       <section className='button_wrapper'>
