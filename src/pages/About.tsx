b805df95a3bb4abe484eba2a04ac40ab1b3a0ea3
gwongibeom <k201gun@kakao.com>
2024-05-10 20:24:18 +0900

    (개발) 어바웃 페이지 개발

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,43 @@
+import Button from '../components/Button';
+import './About.css';
+
 const About = () => {
-  return <div>About</div>;
+  const onMoveHref = (path: string) => {
+    switch (path) {
+      case 'github':
+        window.location.href = 'https://github.com/gwongibeom/wikidocs-lottery';
+        break;
+      case 'gwongibeom':
+        window.location.href = 'https://github.com/gwongibeom';
+        break;
+
+      default:
+        break;
+    }
+  };
+  return (
+    <div>
+      <h1>위키독스 댓글 추첨기</h1>
+      <p>위키독스 책들 페이지에 있는 댓글을 랜덤으로 추첨하는 사이드 프로젝트입니다.</p>
+      <br />
+      <h2>사용법</h2>
+      <ol>
+        <li>
+          <code>https://wikidocs.net/237579</code>에서 <code>237579</code>처럼 페이지 번호를 복사한다.
+        </li>
+        <li>
+          <p>추첨 페이지에 들어가 페이지 번호와 추첨인원을 입력 후 추첨 버튼을 누른다.</p>
+        </li>
+        <li>
+          <p>공유 버튼을 눌러 공유 한다.</p>
+        </li>
+      </ol>
+      <h2>링크</h2>
+      <Button title='깃허브 소스 보러가기' onClick={() => onMoveHref('github')} />
+      <span> </span>
+      <Button title='만든 사람 살펴보기' onClick={() => onMoveHref('gwongibeom')} />
+    </div>
+  );
 };
 
 export default About;
